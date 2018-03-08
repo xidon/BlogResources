88fb2f341946d89274874e2b72c9500f228e4377
Benjamin Parry <benjamin.e.parry@gmail.com>
2018-03-08 23:17:47 +0000

    updated lis_comprehensions.py

--- Python/List_Comprehensions.py
+++ Python/List_Comprehensions.py
@@ -26,10 +26,10 @@ print(lambda_list)
 # for item in iterator:
 #     expression
 
-new_list = []
-for item_a in iterator_a:
-    for item_b in iterator_b:
-        expression eg new_list.append(a*b)
+
+# for item_a in iterator_a:
+#     for item_b in iterator_b:
+#         expression using item_a and item_b
 
 for_list = []
 
@@ -42,6 +42,24 @@ print(for_list)
 comps_list = [x*i for x in "Monty" for i in range(1, 3)]
 
 print(comps_list)
+
+
+my_matrix = [[5, 1, 3], [1, 1, 1], [1, 2, 1]]
+
+my_vector = [1, 2, 3]
+multi_matrix_list = []
+
+for x in my_matrix:
+    temp_list = []
+    for z in range(len(x)):
+        temp_list.append(x[z] * my_vector[z])
+    multi_matrix_list.append(temp_list)
+
+multi_matrix = [[x[z] * my_vector[z] for z in range(len(x))] for x in my_matrix]
+
+print(my_matrix)
+print(multi_matrix_list)
+print(multi_matrix)
 
 # import functools
 # what = list(str(functools.reduce(lambda x,y:x+y,map(lambda x:x**x,range(1, 50))))[-10:])
